63c8733b8960a675fdfe6b5fbc66ed4c5aaf944f
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-01-23 14:53:35 +0000

    creación de comentarios multilínea, comentarios de una línea y función print

---
+++ semana2/programa1.py
@@ -0,0 +1,11 @@
+"""
+    Programa1
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 23/01/2023
+    Descripcion: Comentarios multilinea, 
+    comentarios de una linea y funcion print
+"""
+variable1= "Hola"  #  variable para almacenar una cadena de caracter
+variable2= "mundo python"  #  variable para alamacenar una cadena de caracter
+
+print(variable1 + variable2)
