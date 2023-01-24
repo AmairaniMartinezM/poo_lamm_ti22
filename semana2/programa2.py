c34a48054cab742837a1d419aee3377c78d501fa
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-01-24 14:04:23 +0000

    Se realizo una prueba para verificar los errores que se pueden cometer a la hora de ejecutar algún código

---
+++ semana2/programa2.py
@@ -0,0 +1,29 @@
+"""
+    Programa2
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 24/01/2023
+    Descripcion: Comentarios multilinea, 
+    comentarios de una linea y funcion print
+"""
+
+variable1 = "Hola" #  variable para almacenar una cadena de caracter
+variable2 = 619 #  variable para almacenar una cadena de caracter
+#print(variable1 + variable2)#  variable para almacenar una cadena de caracter
+print("{} {}". format(variable1, variable2))#  variable para almacenar una cadena de caracter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
