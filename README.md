fdc798a90bafe85b53168069094feaa7adaff292
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-01-24 14:58:58 +0000

    Formatear cadenas con # y .format

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 |1|Programa0.py|Hola mundo en Python3|
 |2|Programa1.py|creación de comentarios multilínea, comentarios de una línea y funció|
 |3|programa2.py|.format para cambio de variables|
-|3|programa3.py|Actualización: se realizaron diferentes operaciones y se utilizaron diferentes variables|
+|3.1|programa3.py|Actualización: se realizaron diferentes operaciones y se utilizaron diferentes variables|
--- semana2/programa2.py
+++ semana2/programa2.py
@@ -2,8 +2,7 @@
     Programa2
     Nombre: Luz Amairani Martinez Monroy
     Fecha: 24/01/2023
-    Descripcion: Comentarios multilinea, 
-    comentarios de una linea y funcion print
+    Descripcion: Formatear cadenas con # y .format
 """
 
 variable1 = "Hola" #  variable para almacenar una cadena de caracter
@@ -11,7 +10,7 @@ variable2 = 619 #  variable para almacenar una cadena de caracter
 #print(variable1 + variable2)#  variable para almacenar una cadena de caracter
 print("{} {}". format(variable1, variable2))#  variable para almacenar una cadena de caracter
 
-
+getattr
 
 
 
