f4bf8bd0a043b04eb16deab38ec4e718d59cd868
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-01-30 14:55:00 +0000

    asting para convertir str en int

---
+++ semana3/programa4.py
@@ -0,0 +1,23 @@
+"""
+    programa4
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 30/01/2023
+    Descripcion: Casting para convertir str en int
+"""
+
+numero1 = input("Numero 1: ")
+numero2 = 10
+potencia = int(numero1) ** numero2
+divicion = int(numero1) / numero2
+multiplicacion = int(numero1) * numero2
+resta = int(numero1) - numero2
+suma = int(numero1) + numero2
+print(potencia)
+print(divicion)
+print(multiplicacion)
+print(resta)
+print(suma)
+
+
+
+
