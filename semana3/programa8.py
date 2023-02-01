bbd574a815544a7faab057693820304350017e60
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-02-01 14:52:32 +0000

    Comparar 2 números enteros e imprimir el número mayor

---
+++ semana3/programa8.py
@@ -0,0 +1,15 @@
+"""
+    programa5
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 31/01/2023
+    Descripcion:Comparar 2 numeros entereos e imprimir el numero mayor
+"""
+
+numero1 = int(input("numero1 "))
+numero2 = int(input("numero2 "))
+
+if numero1 > numero2 :
+      print(numero1)
+else:
+      print(numero2)
+    
