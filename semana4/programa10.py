8b45b68977a32e11c4d3373085529f3dfd4d3e79
AmairaniMartine <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-02-08 14:54:35 +0000

    RECUERDA ponerle descripción

---
+++ semana4/programa10.py
@@ -0,0 +1,21 @@
+"""
+    programa10
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 8/02/2023
+    Descripcion:
+"""
+def mayor(numero1, numero2):
+
+	if numero1>numero2:
+		print(numero1)
+
+	elif numero2>numero1:
+	 	print(numero2)
+	else:
+		print("iguales")
+
+mayor(3,2)#3 call
+
+mayor(2,3)#3
+
+mayor(3,3)#None
---
+++ semana4/programa11.py
@@ -0,0 +1,19 @@
+"""
+    programa10
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 8/02/2023
+    Descripcion:
+"""
+def mayor(numero1:int,numero2:int)->int:
+	mayor=None
+	if numero1>numero2:
+		mayor = numero1
+	elif numero2>numero1:
+		mayor = numero2
+	else:
+		mayor=None
+	return mayor 
+print(mayor(3,2))	#3
+print(mayor(2,3))	#3
+print(mayor(3,3))	#None
+        
